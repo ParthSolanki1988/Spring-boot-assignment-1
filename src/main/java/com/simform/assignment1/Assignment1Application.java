734092df8ded9932329c31c3930a5ee0eb129b63
SimformParth <parthsolanki.sp1812@gmail.com>
2023-06-16 09:29:09 +0530

    create demo spring boot application and integration with maven

---
+++ src/main/java/com/simform/assignment1/Assignment1Application.java
@@ -0,0 +1,19 @@
+package com.simform.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * The spring-boot-starter is a Maven dependency provided by Spring Boot that includes a set of common dependencies
+ * and configurations needed to start a Spring Boot application.
+ */
+
+@SpringBootApplication
+public class Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1Application.class, args);
+	}
+
+}
